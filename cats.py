8403e0fab3166e9350f7fb6634509c3aa6ec2791
MegaFlop10 <zac-hah@hotmail.co.uk>
2016-08-22 18:39:29 +1000

    Will turn motor at 9 AM and 5 PM.

--- cats.py
+++ cats.py
@@ -1,8 +1,14 @@
 from datetime import datetime as dt
 import time
 
+import motor
+
+turnTimes = ["9:00:00", "17:00:00"]
+
 checkTime = True
 while checkTime:
     sysTime = str(dt.time(dt.now()))[:8]
+    if sysTime in turnTimes:
+        motor.forward(0.1, 12)
     time.sleep(60)
     print(sysTime)
