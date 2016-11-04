52fbe8fe6261de566ae483c3cd119a9f6934067c
MegaFlop10 <megaflop10@hotmail.com>
2016-11-04 20:13:19 +1100

    works now

--- src/cats.py
+++ src/cats.py
@@ -1,11 +1,11 @@
 import timer
 import motor
-import src.DataLogger as DataLogger
+import DataLogger
 
 motor.init()
 
 while True:
     timer.sleep_remaining_time()
-    motor.forward(0.01, 48)
+    motor.forward(0.1, 12)    # 12 steps / revolution
     motor.stop()  # Saves a little power by turning off the motor coils
     # DataLogger.log_data(sensor_reading)   Will involve vision at some point
