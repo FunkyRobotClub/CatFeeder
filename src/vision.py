4d9aacfa553feca9822e2ed8ea82707e04e65e4e
MegaFlop10 <megaflop10@hotmail.com>
2016-11-08 21:57:28 +1100

    it all works except contourArea makes no sense

--- src/vision.py
+++ src/vision.py
@@ -13,15 +13,17 @@ buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
 image = cv2.imdecode(buff, 1)
 
 retval, dst = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY)
+cv2.imwrite('threshed.jpg', dst)
 mono = cv2.cvtColor(dst, cv2.cv.CV_BGR2GRAY)
-cv2.imwrite('threshed.jpg', mono)
+cv2.imwrite('cvt.jpg', mono)
 blurred = cv2.medianBlur(mono, 5)
 edge = cv2.Canny(blurred, 100, 200)
 list, array = cv2.findContours(edge, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_NONE)
 
-cv2.imwrite('img1.jpg', mono)
+cv2.imwrite('img1.jpg', blurred)
 
 cv2.drawContours(image, list, -1, (0,0,255))
 
-cv2.imwrite('img2.jpg', mono)
+cv2.imwrite('img2.jpg', image)
 
+#print(cv2.contourArea(list))
