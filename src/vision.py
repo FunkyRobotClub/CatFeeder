ae5b19efb0e9c2cbb568306741894ce142ce3c7c
MegaFlop10 <megaflop10@hotmail.com>
2016-11-09 22:16:44 +1100

    Works now, needs cleaning up.

--- src/vision.py
+++ src/vision.py
@@ -13,17 +13,15 @@ buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
 image = cv2.imdecode(buff, 1)
 
 retval, dst = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY)
-cv2.imwrite('threshed.jpg', dst)
 mono = cv2.cvtColor(dst, cv2.cv.CV_BGR2GRAY)
-cv2.imwrite('cvt.jpg', mono)
 blurred = cv2.medianBlur(mono, 5)
 edge = cv2.Canny(blurred, 100, 200)
-list, array = cv2.findContours(edge, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_NONE)
-
-cv2.imwrite('img1.jpg', blurred)
-
+list, hierarchy= cv2.findContours(edge, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_NONE)
 cv2.drawContours(image, list, -1, (0,0,255))
 
-cv2.imwrite('img2.jpg', image)
+for c in list:
+    area = cv2.contourArea(c)
 
-#print(cv2.contourArea(list))
+cv2.putText(image, str(area), (100,100), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0))
+cv2.imwrite('img.jpg', image)
+
